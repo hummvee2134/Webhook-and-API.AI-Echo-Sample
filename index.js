8785c83d8bc7d4072cf983b67f0807d8409ff163
hummvee2134 <25409759+hummvee2134@users.noreply.github.com>
2019-06-14 13:51:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -67,30 +67,3 @@ function PostCode(codestring) {
           console.log('Response: ' + chunk);
       });
   });
-
-  // post the data
-  post_req.write(post_data);
-  post_req.end();
-
-}
-
-// This is an async file read
-fs.readFile('LinkedList.js', 'utf-8', function (err, data) {
-  if (err) {
-    // If this were just a small part of the application, you would
-    // want to handle this differently, maybe throwing an exception
-    // for the caller to handle. Since the file is absolutely essential
-    // to the program's functionality, we're going to exit with a fatal
-    // error instead.
-    console.log("FATAL An error occurred trying to read in the file: " + err);
-    process.exit(-2);
-  }
-  // Make sure there's data before we post it
-  if(data) {
-    PostCode(data);
-  }
-  else {
-    console.log("No data to post");
-    process.exit(-1);
-  }
-});
